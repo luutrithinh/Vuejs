d74c13776e397389c11932b5f985ef783648835a
luutrithinh <luutrithinh23@gmail.com>
2025-03-24 10:59:57 +0700

    https://github.com/luutrithinh/thinh

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Header from './components/header.vue'
+</script>
+
+<template>
+    <Header></Header>
+</template>
+  
---
+++ src/components/header.vue
@@ -0,0 +1,40 @@
+<script>
+</script>
+<template>
+    <div class="Top">
+        <img src="../assets/Logo.png" alt="">
+        <div class="input-item">
+            <i><img src="../assets/Search.png" alt=""></i>
+            <input type="text" class="search" placeholder="Search">
+        </div>
+        <div class="navigation">
+            <p>Home</p>
+            <p>About</p>
+            <p>Contact Us</p>
+            <p>Blog</p>
+        </div>
+        <div class="Icon">
+            <img src="../assets/Favorites.png" alt="">
+            <img src="../assets/Cart.png" alt="">
+            <img src="../assets/User.png" alt="">
+        </div>
+    </div>
+</template>
+<style>
+.Top {
+    display: flex;
+
+}
+.input-item i{
+    position: absolute;
+}
+
+.search {
+    background-color: #F5F5F5;
+
+}
+
+.navigation {
+    display: flex;
+}
+</style>

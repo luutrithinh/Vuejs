5d7ddffa673882f18ccb799fd276d3555fcd051c
luutrithinh <luutrithinh23@gmail.com>
2025-03-25 11:25:18 +0700

    src

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Header from './components/header.vue'
+import Page1 from './components/Page1.vue'
 </script>
 
 <template>
     <Header></Header>
+    <Page1></Page1>
 </template>
   
---
+++ src/components/Page1.vue
@@ -0,0 +1,68 @@
+<script>
+const thinh ="thằng nào đọc cái này là gay";
+console.log(thinh);
+import '../assets/font/stylesheet.css'
+</script>
+
+<template>
+   <div class="Banner">
+    <div class="Content">
+        <p class="Pro">Pro.Beyond</p>
+        <p class="Title">IPhone 14 <span>Pro</span></p>
+        <p class="Cre">Created to change everything for the better. For everyone</p>
+        <button onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">Shop Now</button>
+    </div>
+    <div>
+    <img class="Iphone" src="../assets/Iphone Image.png" alt="">
+   </div>
+   </div>
+   
+</template>
+<style>
+.Banner{
+    color: #5c5a5a;
+    display: flex;
+    font-family: "Inter";
+    padding-left: 30px;    
+    background-color: #211C24;
+    width: 1440px;
+    height: 632px;
+}
+.Pro{
+    font-size: 25px;
+    margin-top: 10px;
+}
+.Title{
+    font-size: 96px;
+    margin: 0;
+}
+.Cre{
+    margin-top: 8px;
+}
+span{
+    font-weight: bold;
+}
+.Content{
+   padding-left: 160px;
+   padding-top: 188px;
+   padding-bottom: 188px;
+}
+.Title{
+    color: white;
+}
+span{
+    font-weight: bolder;
+}
+.Iphone{
+    position: absolute;
+    right: 160px;
+}
+button{
+    border-radius: 10px;
+    border-color: aliceblue;
+    color: aliceblue;
+    width: 191px;
+    height: 56px;
+    background-color: transparent;
+}
+</style>

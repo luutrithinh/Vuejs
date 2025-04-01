2bf5e2ae709fec430bcb957c2b6d41473420e9ba
luutrithinh <luutrithinh23@gmail.com>
2025-04-01 09:25:30 +0700

    src

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -12,6 +12,15 @@ console.log(Testing)
             <img src="../assets/Arrow.png" alt="">
         </div>
         </div>
+        <div class="Cate_Card">
+            <div class="card"><img src="../assets/Cart_icon/Phones.png" alt=""></div>
+            <div class="card"><img src="../assets/Cart_icon/Smart Watches.png" alt=""></div>
+            <div class="card"><img src="../assets/Cart_icon/Cameras.png" alt=""></div>
+            <div class="card"><img src="../assets/Cart_icon/Headphones.png" alt=""></div>
+            <div class="card"><img src="../assets/Cart_icon/Computers.png" alt=""></div>
+            <div class="card"><img src="../assets/Cart_icon/Gaming.png" alt=""></div>
+
+        </div>
     </div>
 </template>
 

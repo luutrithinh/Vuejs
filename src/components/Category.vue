57d6e6a818e909eac028bf8bf838a5112fbd5db2
luutrithinh <luutrithinh23@gmail.com>
2025-04-01 09:02:09 +0700

    src

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -5,13 +5,18 @@ console.log(Testing)
 
 <template>
     <div>
-        <div class="Title_Cate">Browse By Category</div>
-        <div class="pagination">
+        <div class="Title_Cate">
+            <p>Browse By Category</p>
+            <div class="pagination">
             <img src="../assets/Arrow1.png" alt="">
             <img src="../assets/Arrow.png" alt="">
+        </div>
         </div>
     </div>
 </template>
 
 <style>
+.Title_Cate{
+    display: flex;
+}
 </style>

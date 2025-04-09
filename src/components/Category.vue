e553fc7f2217f08be964fcb1fbecb4873d388db5
luutrithinh <luutrithinh23@gmail.com>
2025-04-09 11:37:33 +0700

    src

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -4,28 +4,79 @@ console.log(Testing)
 </script>
 
 <template>
-    <div>
-        <div class="Title_Cate">
-            <p>Browse By Category</p>
-            <div class="pagination">
+    <div class="Cate">
+        <h1 class="Cate_title">Browse By Category</h1>
+        <div class="arrow">
+            <img src="../assets/Arrow.png" alt=""> 
             <img src="../assets/Arrow1.png" alt="">
-            <img src="../assets/Arrow.png" alt="">
         </div>
-        </div>
-        <div class="Cate_Card">
-            <div class="card"><img src="../assets/Cart_icon/Phones.png" alt=""></div>
-            <div class="card"><img src="../assets/Cart_icon/Smart Watches.png" alt=""></div>
-            <div class="card"><img src="../assets/Cart_icon/Cameras.png" alt=""></div>
-            <div class="card"><img src="../assets/Cart_icon/Headphones.png" alt=""></div>
-            <div class="card"><img src="../assets/Cart_icon/Computers.png" alt=""></div>
-            <div class="card"><img src="../assets/Cart_icon/Gaming.png" alt=""></div>
-
+    </div>
+    <div class="Cate_Card">
+        <div class="Card">
+            <div class="Card_content">
+                <img src="../assets/Cart_icon/Cameras.png" alt="">
+                <p>Camera</p>
+            </div>
+            <div class="Card_content">
+                <img src="../assets/Cart_icon/Cameras.png" alt="">
+                <p>Camera</p>
+            </div>
+            <div class="Card_content">
+                <img src="../assets/Cart_icon/Cameras.png" alt="">
+                <p>Camera</p>
+            </div>
+            <div class="Card_content">
+                <img src="../assets/Cart_icon/Cameras.png" alt="">
+                <p>Camera</p>
+            </div>
+            <div class="Card_content">
+                <img src="../assets/Cart_icon/Cameras.png" alt="">
+                <p>Camera</p>
+            </div>
+            <div class="Card_content">
+                <img src="../assets/Cart_icon/Cameras.png" alt="">
+                <p>Camera</p>
+            </div>
+            <!-- <img src="../assets/Cart_icon/Computers.png" alt="">
+            <img src="../assets/Cart_icon/Gaming.png" alt="">
+            <img src="../assets/Cart_icon/Headphones.png" alt="">
+            <img src="../assets/Cart_icon/Phones.png" alt="">
+            <img src="../assets/Cart_icon/Smart Watches.png" alt=""> -->
         </div>
     </div>
 </template>
 
 <style>
-.Title_Cate{
+.Cate {
     display: flex;
+    margin-left: 160px;
+    margin-top: 80px;
+}
+
+.Cate_title {
+    font-size: 25px;
+}
+
+.arrow {
+    display: flex;
+    width: 32px;
+    height: 32px;
+    padding-top: 15px;
+    margin-left: 801px;
+}
+.Card{
+    display: flex;
+}
+.Card_content{
+    padding-top: 24px;
+    text-align: center;
+    background-color: #EDEDED;
+    width: 160px;
+    height: 120px;
+    border-radius: 10px;
+    margin-left: 32px;
+}
+.Cate_Card{
+    margin-left: 100px;
 }
 </style>

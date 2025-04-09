aa9d7883a60c23a076efaee5d201354874b5d7f9
luutrithinh <luutrithinh23@gmail.com>
2025-04-09 13:51:35 +0700

    src

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -4,53 +4,55 @@ console.log(Testing)
 </script>
 
 <template>
-    <div class="Cate">
-        <h1 class="Cate_title">Browse By Category</h1>
-        <div class="arrow">
-            <img src="../assets/Arrow.png" alt=""> 
-            <img src="../assets/Arrow1.png" alt="">
+    <div class="Category">
+        <div class="Cate">
+            <h1 class="Cate_title">Browse By Category</h1>
+            <div class="arrow">
+                <img src="../assets/Arrow1.png" alt="">
+                <img src="../assets/Arrow.png" alt="">
+
+            </div>
         </div>
-    </div>
-    <div class="Cate_Card">
-        <div class="Card">
-            <div class="Card_content">
-                <img src="../assets/Cart_icon/Cameras.png" alt="">
-                <p>Camera</p>
+        <div class="Cate_Card">
+            <div class="Card">
+                <div class="Card_content" onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">
+                    <img src="../assets/Cart_icon/Phones.png" alt="">
+                    <p>Phones</p>
+                </div>
+                <div class="Card_content" onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">
+                    <img src="../assets/Cart_icon/Smart Watches.png" alt="">
+                    <p>Smart Watches</p>
+                </div>
+                <div class="Card_content" onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">
+                    <img src="../assets/Cart_icon/Cameras.png" alt="">
+                    <p>Cameras</p>
+                </div>
+                <div class="Card_content" onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">
+                    <img src="../assets/Cart_icon/Headphones.png" alt="">
+                    <p>Headphones</p>
+                </div>
+                <div class="Card_content" onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">
+                    <img src="../assets/Cart_icon/Computers.png" alt="">
+                    <p>Computers</p>
+                </div>
+                <div class="Card_content" onclick="document.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'">
+                    <img src="../assets/Cart_icon/Gaming.png" alt="">
+                    <p>Gaming</p>
+                </div>
             </div>
-            <div class="Card_content">
-                <img src="../assets/Cart_icon/Cameras.png" alt="">
-                <p>Camera</p>
-            </div>
-            <div class="Card_content">
-                <img src="../assets/Cart_icon/Cameras.png" alt="">
-                <p>Camera</p>
-            </div>
-            <div class="Card_content">
-                <img src="../assets/Cart_icon/Cameras.png" alt="">
-                <p>Camera</p>
-            </div>
-            <div class="Card_content">
-                <img src="../assets/Cart_icon/Cameras.png" alt="">
-                <p>Camera</p>
-            </div>
-            <div class="Card_content">
-                <img src="../assets/Cart_icon/Cameras.png" alt="">
-                <p>Camera</p>
-            </div>
-            <!-- <img src="../assets/Cart_icon/Computers.png" alt="">
-            <img src="../assets/Cart_icon/Gaming.png" alt="">
-            <img src="../assets/Cart_icon/Headphones.png" alt="">
-            <img src="../assets/Cart_icon/Phones.png" alt="">
-            <img src="../assets/Cart_icon/Smart Watches.png" alt=""> -->
         </div>
     </div>
 </template>
 
 <style>
+.Category {
+    background-color: #FAFAFA;
+}
+
 .Cate {
     display: flex;
     margin-left: 160px;
-    margin-top: 80px;
+    padding-top: 80px;
 }
 
 .Cate_title {
@@ -64,10 +66,12 @@ console.log(Testing)
     padding-top: 15px;
     margin-left: 801px;
 }
-.Card{
+
+.Card {
     display: flex;
 }
-.Card_content{
+
+.Card_content {
     padding-top: 24px;
     text-align: center;
     background-color: #EDEDED;
@@ -76,7 +80,8 @@ console.log(Testing)
     border-radius: 10px;
     margin-left: 32px;
 }
-.Cate_Card{
+
+.Cate_Card {
     margin-left: 100px;
 }
 </style>
